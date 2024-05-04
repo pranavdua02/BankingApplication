b18b209b669df735c8e81deb65dfd738137cdf79
pranavdua02 <duapranav11@gmail.com>
2024-05-04 19:20:50 +0530

    Implemented the layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,7 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 
 
 export default async function RootLayout({
@@ -6,11 +10,25 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   
-
+  const loggedIn = {
+    firstName: 'Pranav' , lastName:'Dua'
+  }
   return (
-  <main>
-    SIDEBAR
-    {children} 
+  <main className="flex h-screen w-full font-inter">
+    <Sidebar user={loggedIn}/>
+
+    <div className="flex size-full flex-col">
+        <div className="root-layout">
+           <Image src="/icons/logo.svg" width={30} height={30} alt = "logo"/>
+        
+        <div>
+            <MobileNav user = {loggedIn}/>
+        </div>
+        </div>
+
+        {children}
+    </div>
+     
   </main>
   );
 }

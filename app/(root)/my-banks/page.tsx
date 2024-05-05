d631c56ddac3f6c11dc0565da73453b2007bb154
pranavdua02 <duapranav11@gmail.com>
2024-05-05 20:43:21 +0530

    Implement user actions & plaid functionality

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,7 +1,7 @@
 import BankCard from '@/components/BankCard';
 import HeaderBox from '@/components/HeaderBox'
-//import { getAccounts } from '@/lib/actions/bank.actions';
-//import { getLoggedInUser } from '@/lib/actions/user.actions';
+import { getAccounts } from '@/lib/actions/bank.actions';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react'
 
 const MyBanks = async () => {

1e0de335af265c558b175392804193bfd9a74760
pranavdua02 <duapranav11@gmail.com>
2024-05-06 15:13:22 +0530

    modify progress component

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -1,28 +1,36 @@
-"use client"
+import * as React from "react";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
 
-import * as React from "react"
-import * as ProgressPrimitive from "@radix-ui/react-progress"
+import { cn } from "@/lib/utils";
 
-import { cn } from "@/lib/utils"
+interface ProgressProps {
+  className?: string;
+  value?: number;
+  indicatorClassName?: string;
+  // Add any other props you want to support
+}
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+  ProgressProps
+>(({ className, value, indicatorClassName, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
+    {...props}
     className={cn(
       "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
       className
     )}
-    {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
+      className={cn(
+        "h-full w-full flex-1 bg-primary transition-all",
+        indicatorClassName
+      )}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
-))
-Progress.displayName = ProgressPrimitive.Root.displayName
+));
+Progress.displayName = ProgressPrimitive.Root.displayName;
 
-export { Progress }
+export { Progress };
